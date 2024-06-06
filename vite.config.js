df9742058db29e3d821812930fc257799422904e
Maria <mariaf7458@gmail.com>
2024-06-06 11:46:30 -0500

    Revert "Vite config, package.json"
    This reverts commit 8c044fe3b8b8b4a16f8ed99e0d18e901ce8a37d0.

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mriatorres__3d_portfolio/',
-  build: {
-    chunkSizeWarningLimit: 1600,
-    mime: {
-      '.js': 'application/javascript',
-      '.css': 'text/css',
-    },
-  },
-  server: {
-    middleware: [
-      {
-        async handle(req, res, next) {
-          if (req.url.endsWith('.js')) {
-            res.setHeader('Content-Type', 'application/javascript');
-          } else if (req.url.endsWith('.css')) {
-            res.setHeader('Content-Type', 'text/css');
-          }
-          return next();
-        },
-      },
-    ],
-  },
 })
